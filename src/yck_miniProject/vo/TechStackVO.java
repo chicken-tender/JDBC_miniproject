98ccb8018c1c0e60a8bee66ba6feeb228730430b
dekim0705 <dekim0705@gmail.com>
2023-04-20 20:03:18 +0900

    GradeVo, MemberTechStackVO, TechStackVO update

--- src/yck_miniProject/vo/TechStackVO.java
+++ src/yck_miniProject/vo/TechStackVO.java
@@ -1,4 +1,43 @@
 package yck_miniProject.vo;
 
 public class TechStackVO {
+    private int stackNum;
+    private String stackIconUrl;
+    private String stackName;
+
+    public TechStackVO(int stackNum, String stackIconUrl, String stackName) {
+    this.stackNum = stackNum;
+    this.stackIconUrl = stackIconUrl;
+    this.stackName = stackName;
+    }
+
+    public TechStackVO() {
+
+    }
+
+    public int getStackNum() {
+        return stackNum;
+    }
+
+    public void setStackNum(int stackNum) {
+        this.stackNum = stackNum;
+    }
+
+    public String getStackIconUrl() {
+        return stackIconUrl;
+    }
+
+    public void setStackIconUrl(String stackIconUrl) {
+        this.stackIconUrl = stackIconUrl;
+    }
+
+    public String getStackName() {
+        return stackName;
+    }
+
+    public void setStackName(String stackName) {
+        this.stackName = stackName;
+    }
 }
+
+
